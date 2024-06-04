0272d69d52ce38c520bf08b27ae9d266b77ffb30
TAN XUN LIANG <1201100967@student.mmu.edu.my>
2024-06-04 10:54:15 +0800

    A+B+C

--- app/src/main/java/com/example/quiz2/MainActivity.kt
+++ app/src/main/java/com/example/quiz2/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.quiz2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val editText1 = findViewById<EditText>(R.id.editTextNumberDecimal1)
+        val editText2 = findViewById<EditText>(R.id.editTextNumberDecimal3)
+        val editText3 = findViewById<EditText>(R.id.editTextNumberDecimal4)
+        val buttonAdd = findViewById<Button>(R.id.button7)
+        val buttonSubtract = findViewById<Button>(R.id.button8)
+        val buttonMultiply = findViewById<Button>(R.id.button9)
+        val textViewResult = findViewById<TextView>(R.id.textView2)
+
+        buttonAdd.setOnClickListener {
+            val number1 = editText1.text.toString().toDoubleOrNull() ?: 0.0
+            val number2 = editText2.text.toString().toDoubleOrNull() ?: 0.0
+            val number3 = editText3.text.toString().toDoubleOrNull() ?: 0.0
+
+            val result = (number1 + number2 + number3).toInt()
+            textViewResult.text = "Result: $result"
+        }
+
     }
 }
